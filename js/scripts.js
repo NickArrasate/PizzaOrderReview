a0fcdb13205ebdac6933e2f3506a5f502c2c1bdb
Nick <narrasate22@hotmail.com>
2016-04-08 16:45:38 -0700

    finalize styling. remove whitespace and properly indent code. final commit for submission to review.

--- js/scripts.js
+++ js/scripts.js
@@ -4,40 +4,43 @@ function Pizza(size, ingredients) {
   this.ingredients = ingredients;
   this.total = 0;
 }
-
+function Address(name, street, city, state) {
+  this.name = name;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
 Pizza.prototype.getPrice = function(){
   var ingTotal = 0;
-    this.ingredients.forEach(function(ingredient){
-       ingTotal = ingTotal += (parseInt(ingredient));
-    })
-    return parseInt(ingTotal) + this.size;
+  this.ingredients.forEach(function(ingredient){
+    ingTotal = ingTotal += (parseInt(ingredient));
+  })
+  return parseInt(ingTotal) + this.size;
+}
+Address.prototype.fullAddress = function(){
+  return "Thank you, " + this.name + "." + " Your order is on it's way to " + this.street + " " + this.city + ", " + this.state + ".";
 }
 function getIngredients() {
-    ingredients = $('.ingPick:checked').map(function() {return this.value;}).get();
-    return ingredients;
-    };
-
+  ingredients = $('.ingPick:checked').map(function() {return this.value;}).get();
+  return ingredients;
+};
 function getSize(){
   var e = document.getElementById("sizeselect");
-   size = e.options[e.selectedIndex].text;
-
-   if (size == 'Small'){
-     size = 12;
-   } else if (size == 'Medium'){
-     size = 14;
-   } else if (size == 'Large'){
-     size = 16;
-   } else if (size == 'XL'){
-     size = 18;
-   }
+  size = e.options[e.selectedIndex].text;
+  if (size == 'Small'){
+    size = 12;
+  } else if (size == 'Medium'){
+    size = 14;
+  } else if (size == 'Large'){
+    size = 16;
+  } else if (size == 'XL'){
+    size = 18;
+  }
   return size;
 };
-
-
 // user interface logic
 var size = null;
 var ingredients = [];
-
 $(document).ready(function() {
   $("form#pizzaform").submit(function(event) {
     event.preventDefault();
@@ -48,5 +51,19 @@ $(document).ready(function() {
     $('#orderdisplay').show();
     $('#orderprice').text(newPizza.total);
   });
-
+  $('#delivery').click(function(){
+    $('#delivery').hide();
+    $('.addressGetter').show();
+  });
+  $('form.addressGetter').submit(function(event){
+    event.preventDefault();
+    $(".addressGetter").each(function() {
+      var inputtedName = $(this).find("input.name").val();
+      var inputtedStreet = $(this).find("input.street").val();
+      var inputtedCity = $(this).find("input.city").val();
+      var inputtedState = $(this).find("input.state").val();
+      var newAddress = new Address(inputtedName, inputtedStreet, inputtedCity, inputtedState);
+      $('#addressDisplay').text(newAddress.fullAddress());
+    });
+  });
 });
